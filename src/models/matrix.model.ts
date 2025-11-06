da9ac4c9fd254b17497a6d9e1d9f5d41949fae94
HNK-005 <huynhnamkha1010@gmail.com>
2025-11-07 00:04:41 +0700

    SV-4 completed ui for visualiztion alrogithm

--- src/models/matrix.model.ts
+++ src/models/matrix.model.ts
@@ -1,35 +1,35 @@
-import { NodeModel } from "./node.model";
+import { NodeModel } from './node.model';
 
 export class MatrixModel {
-    private nodes: NodeModel[][];
-    private numRows: number;
-    private numCols: number;
+  private nodes: NodeModel[][];
+  private numRows: number;
+  private numCols: number;
 
-    constructor(numRows: number, numCols: number) {
-        this.numRows = numRows;
-        this.numCols = numCols;
-        this.nodes = [];
-        this.initializeMatrix();
-    }
+  constructor(numRows: number, numCols: number) {
+    this.numRows = numRows;
+    this.numCols = numCols;
+    this.nodes = [];
+    this.initializeMatrix();
+  }
 
-    initializeMatrix(): void {
-        for (let row = 0; row < this.numRows; row++) {
-            const currentRow: NodeModel[] = [];
-            for (let col = 0; col < this.numCols; col++) {
-                currentRow.push(new NodeModel(row, col));
-            }
-            this.nodes.push(currentRow);
-        }
+  initializeMatrix(): void {
+    for (let row = 0; row < this.numRows; row++) {
+      const currentRow: NodeModel[] = [];
+      for (let col = 0; col < this.numCols; col++) {
+        currentRow.push(new NodeModel(row, col));
+      }
+      this.nodes.push(currentRow);
     }
+  }
 
-    public getNode(row: number, col: number): NodeModel {
-        return this.nodes[row][col];
-    }
+  public getNode(row: number, col: number): NodeModel {
+    return this.nodes[row][col];
+  }
 
-    public getNumRows(): number {
-        return this.numRows;
-    }
-    public getNumCols(): number {
-        return this.numCols;
-    }
+  public getNumRows(): number {
+    return this.numRows;
+  }
+  public getNumCols(): number {
+    return this.numCols;
+  }
 }
